da41ff74478ebb760f8d048fb4c4891425f9da04
ThibautBaudry <thibaut.baudry@gmail.com>
2026-03-11 15:34:32 +0100

    MultisigGovernanceStorage library added to the Storage libraries

--- src/libraries/StorageLibraries.sol
+++ src/libraries/StorageLibraries.sol
@@ -53,3 +53,17 @@ library ConfigStorage {
         assembly {l.slot := slot}
     }
 }
+
+library MultisigGovernanceStorage {
+    bytes32 internal constant STORAGE_SLOT =
+    keccak256("mostro.storage.multisig.governance");
+
+    function layout()
+    internal
+    view
+    returns (IMostroStructs.MultisigGovernanceLayout storage l)
+    {
+        bytes32 slot = STORAGE_SLOT;
+        assembly {l.slot := slot}
+    }
+}
